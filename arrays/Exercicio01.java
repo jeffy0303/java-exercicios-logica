1ab8786406545fb804e6ad8af7360e4bc992a38d
jeffy0303 <limajeff00@gmail.com>
2026-04-07 22:43:53 -0400

    Exercicio 01 - lista de nomes

---
+++ arrays/Exercicio01.java
@@ -0,0 +1,16 @@
+public class Exercicio01{
+    public static void main (String[] args){
+    
+    String [] nomes = new String [4];
+     
+    nomes [0] = "Maria";
+    nomes [1] = "Alexia";
+    nomes [2] = "Elias";
+    nomes [3] = "Lorenzo";
+    
+    //for (int i = 0; i < nomes.length; i++){
+    for (String i: nomes){
+    System.out.println(i);}
+    
+    }    
+}
